4f4ef87d90f26d8cc8430f2622877fc96e9cc9dd
Mor Shemesh <mor.shemesh@gmail.com>
2022-05-25 09:17:44 +0300

    add cypress test for suspense

--- e2e/app.e2e.ts
+++ e2e/app.e2e.ts
@@ -1,6 +1,16 @@
 describe('Demo App E2E', () => {
-  it('should load the page', () => {
+  it('should load home page', () => {
     cy.visit('http://localhost:3000/');
     cy.findByTestId('page-home').should('exist');
+  });
+
+  it('should show `Loading...` before i18n resources were loaded', () => {
+    cy.intercept('/translations/messages.en.json', async (req) => {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      req.continue();
+    });
+
+    cy.visit('http://localhost:3000/', {});
+    cy.findByText('Loading...').should('exist');
   });
 });
