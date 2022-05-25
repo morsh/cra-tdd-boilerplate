0d979281e1615a3c76f251e974240ca64214898f
Mor Shemesh <mor.shemesh@gmail.com>
2022-05-25 09:04:49 +0300

    fix cypress test id

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
+import './testing-library';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
